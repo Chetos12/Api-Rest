a514824c7c2346f008c36f52bb775bdeba8d3dde
Lester Estuardo Carrera <toonick.lester.estuardo@gmail.com>
2016-05-18 13:42:45 -0500

    Connecting MongoDB

--- app.js
+++ app.js
@@ -3,6 +3,16 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
+var mongoose = require('mongoose');
+
+//Conexion con MongoDB
+mongoose.connect('mongodb://localhost/serietv',function (err,res) {
+	if(err){
+		console.log('ERROR: Conexion con la base de Datos '+err)
+	}else{
+		console.log('Conexion con la Base de datos Establecida');
+	}
+})
 
 //Requiriendo Modulos de Rutas
 var routerIndex = require('./routes/index');
