d1c5a651c90ffc9d5d6fb6969a7966bf87c28791
Lester Estuardo Carrera <toonick.lester.estuardo@gmail.com>
2016-05-19 09:38:28 -0500

    Controller tv shows

---
+++ controllers/tvshow.js
@@ -0,0 +1,71 @@
+var mongoose = require('mongoose');
+var TVShow = require('../models/tvshow');
+
+//Reciev GET
+exports.finAllTvShows= function (req, res) {
+    TVShow.find(function (err, data) {
+        if(err){
+            res.send(500,err.message);
+        }else{
+            console.log('GET/tvshows');
+            res.status(200).jsonp(data);
+        }
+    });
+};
+
+//Reciev POST
+
+exports.addTvShow = function (req, res) {
+    console.log('POST');
+    console.log(req.body);
+
+    var newTvShow = new TVShow({
+        title:req.body.title,
+        year:req.body.year,
+        country:req.body.country,
+        poster:req.body.poster,
+        seasons:req.body.seasons,
+        genre:req.body.genre,
+        summary:req.body.summary
+    });
+
+    newTvShow.save(function (err,data) {
+        if(err){
+            return res.status(500).send(err.message);
+        }else {
+            res.status(200).jsonp(data);
+        }
+    });
+};
+
+exports.updateTvShow = function (req, res) {
+    TVShow.findById(req.params.id,function (err,data) {
+        data.title = req.body.petId;
+        data.year = req.body.year;
+        data.country = req.body.country;
+        data.poster = req.body.poster;
+        data.seasons = req.body.seasons;
+        data.genre = req.body.genre;
+        data.summary = req.body.summary;
+
+        data.save(function (err) {
+           if(err){
+               return res.status(500).send(err.message);
+           } else{
+               res.status(200).jsonp(data);
+           }
+        });
+    });
+};
+
+exports.deleteTvShow = function (req, res) {
+    TVShow.findById(req.params.id,function (err, data) {
+        data.remove(function (err) {
+            if(err){
+                return res.status(500).send(err.message);
+            }else{
+                res.status(200).send();
+            }
+        });
+    });
+}
--- routes/api.js
+++ routes/api.js
@@ -3,5 +3,4 @@ var router = express.Router();
 router.get('/',function (req, res) {
     res.send("Bienvenido a la Api");
 });
-
 module.exports = router;
